ab55f01750b4082dc6c98a946e30e6bd22ba292b
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-02 00:50:12 +0300

    Message count is now displaying via a variable

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import { useState, useEffect } from 'react';
 const messages = {
   "tr-TR": {
     title: "Merhaba, Dünya!",
-    description: "3 yeni mesajınız var!",
+    description: "{count} yeni mesajınız var!",
   },
   "en-US": {
     title: "Hello, World!",
-    description: "You have 3 new messages!",
+    description: "You have {count} new messages!",
   }
 }
 
@@ -40,7 +40,7 @@ function App() {
         <FormattedMessage id="title" />
 
         <p>
-          <FormattedMessage id="description" />
+          <FormattedMessage id="description" values={{count: 5}} />
         </p>
 
         <br /><br />
